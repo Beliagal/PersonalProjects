90d3003d8de03489117cec5e52f9ef35dbf7875c
beliagal <beliagal@hotmail.com>
2020-12-09 19:35:21 +0100

    first commit - version 1

---
+++ MyProject/characters.py
@@ -0,0 +1,15 @@
+class Character():
+
+    def __init__ (self, name, character_class, alignment, level, race, px):
+
+        self.name = name
+        self.character_class = character_class
+        self.alignment = alignment
+        self.level = level
+        self.race = race
+        self.px = px
+
+    def start_game (self)
+
+Player1 = Character("Legolas", "Ranger", "Legal", 1, "Elf", 0)
+myCharacter = 
--- scrapping.py
+++ scrapping.py
@@ -5,6 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import unicodedata
+import collections
 
 url = "https://elpais.com/economia/2020/11/25/actualidad/1606309651_814788.html" 
 page = requests.get(url)
@@ -20,7 +21,7 @@ document.close
 document_text = open ('file_to_scrap.txt', 'r')
 text_string = document_text.read().lower()
 output = unicodedata.normalize('NFD', text_string).encode('ascii', 'ignore')
-match_pattern = re.findall(r'[a-z]{1,30}', str(output))
+match_pattern = str(re.findall(r'[a-z]{1,30}', str(output)))
 
 class words_frequency():
 
@@ -30,16 +31,9 @@ class words_frequency():
 
     def counter(self):
 
-        frequency = {}
-        
-        for word in match_pattern:
-            count = frequency.get(word,0)
-            frequency[word] = count + 1
-
-        frequency_list = sorted(frequency.keys())
-
-        for words in frequency_list:
-            print (words, frequency[words])
+        counter_words = collections.Counter(match_pattern.split())
+        for word, cont in counter_words.most_common():
+            print(f"<{word}> has been written {cont} {'times' if cont > 1 else 'time'}.")
 
 myCounter = words_frequency('file_to_scrap.txt')
 myCounter.counter()
